a2daa4b0d3fe9424d2707d015d99e025c088c473
Enrique Lobo <enriquealfenas@gmail.com>
2024-03-09 00:00:00 -0300

    Num aguento mais

--- loop/src/loop/Categoria.java
+++ loop/src/loop/Categoria.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package loop;
 
-/**
- *
- * @author 10414032675
- */
 public class Categoria {
     
     int qtdPlayer;
